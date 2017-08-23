f1fb303a73152c92d2af7033441bb3aad49b9c55
kser3221 <kser3221@gmail.com>
2017-08-23 17:48:08 +0900

    kser3221 firebase

--- src/src-firebase/environments/environment.ts
+++ src/src-firebase/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBJTlEpv3Gd8J7j4h0LnVWkehcpuOt3vfQ",
-    authDomain: "tour-of-heroes-23ebb.firebaseapp.com",
-    databaseURL: "https://tour-of-heroes-23ebb.firebaseio.com",
-    projectId: "tour-of-heroes-23ebb",
-    storageBucket: "tour-of-heroes-23ebb.appspot.com",
-    messagingSenderId: "191080138084"
+    apiKey: "AIzaSyDmKTmTJ1q22AFqUZRISQ1jz8-g2iVacoQ",
+    authDomain: "tour-of-heroes-afd13.firebaseapp.com",
+    databaseURL: "https://tour-of-heroes-afd13.firebaseio.com",
+    projectId: "tour-of-heroes-afd13",
+    storageBucket: "tour-of-heroes-afd13.appspot.com",
+    messagingSenderId: "995536334558"
   }
 };
