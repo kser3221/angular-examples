694236f2aa8945ed4db37c7b68eca2a218b86352
kser3221 <kser3221@gmail.com>
2017-09-20 16:37:13 +0900

    add ignored, force cache for unchanging files, disable ghostMode

--- bs-config.js
+++ bs-config.js
@@ -1,10 +1,19 @@
 module.exports = {
   browser: "firefox",
+  watchOptions: {
+	  ignored: ["node_modules", "jspm_packages", ".vscode"]
+  },
   server: {
     baseDir: "src",
     "routes": {
       "/jspm_packages": "jspm_packages"
     },
+	// only applied for baseDir(src/**)
+	// not applied for routes(jspm_packages)
+	serveStaticOptions: {
+		etag: true,         // default : true
+		cacheControl: true  // default : true
+	},
     middleware: {
       // overrides the second middleware default with new settings
       // refer: https://github.com/bripkens/connect-history-api-fallback
@@ -29,7 +38,20 @@ module.exports = {
           { from: /\/reversi.*/, to: '/reversi.html' }
         ],
         verbose: false
-      })
+      }),
+	  /* DO NOT CHECK for not-changing files */
+	  2: function(req, res, next) {
+		  if (/^\/jspm_packages/.test(req.url)) {
+			  res.setHeader('Cache-Control', 'public, max-age=315360000');
+			  res.setHeader('Expires', 'Mon, 01 JAN 2029 00:00:00 GMT');
+		  }
+		  next();
+	  }
     }
+  },
+  ghostMode: {
+	  clicks: false,
+	  forms: false,
+	  scroll: false
   }
 };
