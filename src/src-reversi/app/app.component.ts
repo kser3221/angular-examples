f9c4b4142e5f8d6e105ed1422e90b39301ec3f90
kser3221 <kser3221@gmail.com>
2017-09-20 13:56:27 +0900

    ex1

--- src/src-reversi/app/app.component.ts
+++ src/src-reversi/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+class Cell {
+  cellStatus: '';
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Hello World';
+export class AppComponent implements OnInit {
+  title = 'Hello';
+  white = 0;
+  black = 0;
+  cells:Cell[] = [];
+  constructor(){}
+
+  ngOnInit() {
+    for(let i = 0; i < 64; i++) {
+      this.cells.push({cellStatus: ''});
+    }
+  }
+  
+  cellClick(ind:number) {
+  }
 }
